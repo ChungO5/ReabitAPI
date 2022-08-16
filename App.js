8b77d214aea54306071d2c2bf5e85a3ab7fe5da6
KimChungO <kco796@naver.com>
2022-08-16 14:59:49 +0900

    First

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+import HomeScreen from './src/Screen/HomeScreen';
+
+const App = () => {
+    return <HomeScreen></HomeScreen>;
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
---
+++ src/Screen/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, TextInput, View} from 'react-native';
+
+const HomeScreen = () => {
+    return (
+        <View>
+            <TextInput></TextInput>
+        </View>
+    );
+};
+
+export default HomeScreen;
